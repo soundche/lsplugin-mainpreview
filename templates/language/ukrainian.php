d359bfbf7638f599a0e4f017c29a7202522899e2
Полищук Сергей <spolischook@spolischook.(none)>
2012-11-18 03:50:34 +0200

    add ukrainian language

---
+++ templates/language/ukrainian.php
@@ -0,0 +1,36 @@
+<?php
+/*-------------------------------------------------------
+*
+*   LiveStreet Engine Social Networking
+*   Copyright © 2008 Mzhelskiy Maxim
+*
+*--------------------------------------------------------
+*
+*   Official site: www.livestreet.ru
+*   Contact e-mail: rus.engine@gmail.com
+*
+*   GNU General Public License, version 2:
+*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*
+---------------------------------------------------------
+*/
+
+/**
+ * Українська мова для плагіна
+ */
+return array(
+	'admin_list_convert' => 'Провести пошук та автоматичну генерацію прев’ю для топіків',
+	'admin_list_reconvert' => 'Провести переконвертацію усіх прев’ю до нових розмірів із конфігу',
+	'submit_convert' => 'Запустити генерацію прев’ю',
+	'submit_convert_notice' => 'Увага! Якщо у вас багато статтей, то процес генерації може зайняти багато часу. ',
+	'submit_reconvert' => 'Запустити перегенерацію прев’ю',
+	'submit_reconvert_notice' => 'Увага! Якщо у вас багато статтей, то процес перегенерації може зайняти багато часу. ',
+	'convert_end' => 'Перевірено статтей %%count_topic%%',
+	'reconvert_end' => 'Переконвертовано статтей %%count_topic%%',
+	'form_preview_image_delete' => 'Видалити прев’ю',
+	'form_preview_image_delete_auto' => 'автоматичне',
+	'form_preview_image' => 'Прев’ю статті',
+	'form_preview_image_notice' => 'Ви можете завантажити зображення, яке буде прев’ю статті на головній сторінці',
+);
+
+?>
